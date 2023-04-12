f8d82aaca3be276fd09aa7dcb700dca754d4574a
uczen <uczen@TE-PIO-D-307M14.local>
2023-04-12 11:03:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [btn1, setBtn1] = useState(false);
+    const [btn2, setBtn2] = useState(true);
+    const [wynik, setWynik] = useState("");
+    const [style, setStyle] = useState("");
+    const [styleTak, setStyleTak] = useState("niewidac");
+    const [licznik, setLicznik] = useState(0);
+
+    const handleBtn1 = () => {
+        setBtn2(!btn2);
+        setBtn1(!btn1);
+        setStyle("btn2");
+        setLicznik(licznik + 1);
+    };
+    const handleBtn2 = () => {
+        setBtn2(!btn2);
+        setBtn1(!btn1);
+        setLicznik(licznik + 1);
+    };
+
+    return (
+        <div>
+            <h1>Jesteś głupi(a)?</h1>
+            <div className={licznik === 9 ? "niewidac" : "btn1"}>
+                {btn1 && (
+                    <button
+                        onMouseEnter={handleBtn1}
+                        onClick={() => setWynik("Oczywiscie ze nie jestes")}
+                    >
+                        Nie
+                    </button>
+                )}
+            </div>
+            <button
+                className={licznik < 9 && styleTak}
+                onClick={() => setWynik("Wiedziałem ze tak")}
+            >
+                Tak
+            </button>
+            <div className={style}>
+                {btn2 && (
+                    <button
+                        onMouseEnter={handleBtn2}
+                        onClick={() => setWynik("Oczywiscie ze nie jestes")}
+                    >
+                        Nie
+                    </button>
+                )}
+            </div>
+
+            <h1 className="pos">{wynik}</h1>
+        </div>
+    );
+};
 
 export default App;
